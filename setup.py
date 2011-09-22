3d5ce3ccf2638e107fab3e7e434e6f6f2f78dc49
Ben McCormack <benm@fogcreek.com>
2011-09-22 17:19:45 +0400

    adding setup.py so that FogBugzPy functions more like a standard Python package.

---
+++ setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+
+setup(name='fogbugz',
+      version='0.9.0',
+      py_modules=['fogbugz']
+      description='Python library for interacting with the FogBugz API',
+      author='Tyler Hicks-Wright',
+      author_email='customer-service@fogcreek.com',
+      maintainer='Fog Creek Software'
+      maintainer_email='customer-service@fogcreek.com',
+      url='https://developers.kilnhg.com/Repo/FogBugz/Group/FogBugzPy',
+      download_url='https://developers.kilnhg.com/Repo/FogBugz/Group/FogBugzPy'
+      )
