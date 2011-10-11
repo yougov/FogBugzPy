8233808e70f96dfd1560ab05ab2d933d84431b1f
Tyler Hicks-Wright <tghw@fogcreek.com>
2011-10-11 13:50:26 -0200

    We now tell the installer to grab BeautifulSoup, no need for us to include it.

--- setup.py
+++ setup.py
@@ -3,12 +3,12 @@ from distutils.core import setup
 setup(name='fogbugz',
       version='0.9.0',
       py_modules=['fogbugz'],
-      packages=['BeautifulSoup'],      
       description='Python library for interacting with the FogBugz API',
-      author='Tyler Hicks-Wright',
+      author='Tyler G. Hicks-Wright',
       author_email='customer-service@fogcreek.com',
       maintainer='Fog Creek Software',
       maintainer_email='customer-service@fogcreek.com',
       url='https://developers.kilnhg.com/Repo/FogBugz/Group/FogBugzPy',
-      download_url='https://developers.kilnhg.com/Repo/FogBugz/Group/FogBugzPy'
+      download_url='https://developers.kilnhg.com/Repo/FogBugz/Group/FogBugzPy',
+      install_requires=['BeautifulSoup==3.2'],
       )
