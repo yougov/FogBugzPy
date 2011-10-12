8e35d3b84932133e77b681a6a431514de906fb80
Tyler Hicks-Wright <tghw@fogcreek.com>
2011-10-12 06:25:33 -0200

    Updating version to 0.9.1

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ use_setuptools()
 from setuptools import setup
 
 setup(name='fogbugz',
-      version='0.9.0',
+      version='0.9.1',
       py_modules=['fogbugz'],
       description='Python library for interacting with the FogBugz API',
       author='Tyler G. Hicks-Wright',
